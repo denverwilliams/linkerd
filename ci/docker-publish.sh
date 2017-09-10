e69dc58d92ae4d378f55571dcc31c217ceba0a51
dlx <denver@ii.coop>
2017-09-11 07:36:58 +1200

    Only set DockerTag

--- ci/docker-publish.sh
+++ ci/docker-publish.sh
@@ -7,7 +7,7 @@ set -eu
 tag=""
 if [ -n "${1:-}" ]; then
     tag="$1"
-    registry="registry.cncf.ci/linkerd/linkerd"
+    # registry="registry.cncf.ci/linkerd/linkerd"
 fi
 
 # if DOCKER_CREDENTIALS is set, save it locally.
@@ -23,9 +23,9 @@ if [ "${NO_PUSH:-}" = "1" ]; then
 fi
 
 if [ -n "$tag" ]; then
-    ./sbt "set Base.dockerRegistry in (linkerd, Bundle) := \"${tag}\"" "set Base.dockerRegistry in (linkerd, Bundle) := \"${registry}\"" "linkerd/bundle:${docker_target}" \
-          "set Base.dockerTag in (namerd, Bundle) := \"${tag}\"" "set Base.dockerRegistry in (namerd, Bundle) := \"${registry}\"" "namerd/bundle:${docker_target}" \
-          "set Base.dockerTag in (namerd, Dcos) := \"dcos-${tag}\"" "set Base.dockerRegistry in (namerd, Dcos) := \"${registry}\"" "namerd/dcos:${docker_target}"
+    ./sbt "set Base.dockerTag in (linkerd, Bundle) := \"${tag}\"" "linkerd/bundle:${docker_target}" \
+          "set Base.dockerTag in (namerd, Bundle) := \"${tag}\"" "namerd/bundle:${docker_target}" \
+          "set Base.dockerTag in (namerd, Dcos) := \"dcos-${tag}\"" "namerd/dcos:${docker_target}"
 else
   ./sbt "linkerd/bundle:${docker_target}" \
         "namerd/bundle:${docker_target}" \
